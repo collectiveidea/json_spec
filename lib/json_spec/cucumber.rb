534bfd1e1bd457c9119e18b7d7cadd4d3440f320
Steve Richert <steve.richert@gmail.com>
2011-07-08 17:08:41 -0400

    Add negative option to Cucumber steps

--- lib/json_spec/cucumber.rb
+++ lib/json_spec/cucumber.rb
@@ -2,23 +2,43 @@ require File.expand_path("../../json_spec", __FILE__)
 
 World(JsonSpec::Helpers)
 
-Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should be:$/ do |path, json|
-  last_json.should be_json_eql(json).at_path(path)
+Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should( not)? be:$/ do |path, negative, json|
+  if negative
+    last_json.should_not be_json_eql(json).at_path(path)
+  else
+    last_json.should be_json_eql(json).at_path(path)
+  end
 end
 
-Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should be (".*"|\-?\d+(?:\.\d+)?(?:[eE][\+\-]?\d+)?|\[.*\]|\{.*\}|true|false|null)$/ do |path, value|
-  last_json.should be_json_eql(value).at_path(path)
+Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should( not)? be (".*"|\-?\d+(?:\.\d+)?(?:[eE][\+\-]?\d+)?|\[.*\]|\{.*\}|true|false|null)$/ do |path, negative, value|
+  if negative
+    last_json.should_not be_json_eql(value).at_path(path)
+  else
+    last_json.should be_json_eql(value).at_path(path)
+  end
 end
 
-Then /^the (?:JSON|json)(?: response)? should have "(.*)"$/ do |path|
-  last_json.should have_json_path(path)
+Then /^the (?:JSON|json)(?: response)? should( not)? have "(.*)"$/ do |negative, path|
+  if negative
+    last_json.should_not have_json_path(path)
+  else
+    last_json.should have_json_path(path)
+  end
 end
 
-Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should be an? (.*)$/ do |path, type|
+Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should( not)? be an? (.*)$/ do |path, negative, type|
   klass = Module.const_get(type.gsub(/^./){|x| x.upcase })
-  last_json.should have_json_type(klass).at_path(path)
+  if negative
+    last_json.should_not have_json_type(klass).at_path(path)
+  else
+    last_json.should have_json_type(klass).at_path(path)
+  end
 end
 
-Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should have (\d+)/ do |path, size|
-  last_json.should have_json_size(size.to_i).at_path(path)
+Then /^the (?:JSON|json)(?: response)?(?: at "(.*)")? should( not)? have (\d+)/ do |path, negative, size|
+  if negative
+    last_json.should_not have_json_size(size.to_i).at_path(path)
+  else
+    last_json.should have_json_size(size.to_i).at_path(path)
+  end
 end
