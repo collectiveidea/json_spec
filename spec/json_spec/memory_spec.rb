75dbb3a879f1ec4309a0fc2adc1574b20a91c6af
Steve Richert <steve.richert@gmail.com>
2011-07-09 10:32:42 -0400

    Test JSON memory

---
+++ spec/json_spec/memory_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe JsonSpec::Memory do
+  it "has a memory" do
+    JsonSpec.memory.should == {}
+  end
+
+  it "memorizes strings" do
+    JsonSpec.memorize("key", "value")
+    JsonSpec.memory.should == {"key" => "value"}
+  end
+
+  it "regurgitates unremembered strings" do
+    JsonSpec.remember("foo%{bar}").should == "foo%{bar}"
+  end
+
+  it "remembers strings" do
+    JsonSpec.memorize("bar", "baz")
+    JsonSpec.remember("foo%{bar}").should == "foobaz"
+  end
+
+  it "forgets" do
+    JsonSpec.memorize("key", "value")
+    JsonSpec.forget
+    JsonSpec.memory.should == {}
+  end
+end
