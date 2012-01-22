72f72ac88e3f02c369133f465c0ccf77d936c564
Steve Richert <steve.richert@gmail.com>
2012-01-22 12:28:32 -0500

    Reorder dependent file requires

--- lib/json_spec.rb
+++ lib/json_spec.rb
@@ -2,8 +2,8 @@ require "json_spec/version"
 require "json_spec/errors"
 require "json_spec/configuration"
 require "json_spec/exclusion"
-require "json_spec/matchers"
 require "json_spec/helpers"
+require "json_spec/matchers"
 require "json_spec/memory"
 
 module JsonSpec
