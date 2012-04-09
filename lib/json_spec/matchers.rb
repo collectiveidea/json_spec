b95b570564b6a33e500310824fff52e9f798ac27
Boris Staal <boris@roundlake.ru>
2012-04-10 02:25:08 +0400

    HaveJsonFields matcher

--- lib/json_spec/matchers.rb
+++ lib/json_spec/matchers.rb
@@ -3,6 +3,7 @@ require "json_spec/matchers/include_json"
 require "json_spec/matchers/have_json_path"
 require "json_spec/matchers/have_json_type"
 require "json_spec/matchers/have_json_size"
+require "json_spec/matchers/have_json_fields"
 
 module JsonSpec
   module Matchers
@@ -24,6 +25,10 @@ module JsonSpec
 
     def have_json_size(size)
       JsonSpec::Matchers::HaveJsonSize.new(size)
+    end
+
+    def have_json_fields(size)
+      JsonSpec::Matchers::HaveJsonFields.new(size)
     end
   end
 end
---
+++ spec/json_spec/matchers/have_json_fields_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe JsonSpec::Matchers::HaveJsonFields do
+  it "fails for non-hashes" do
+    %([1,2,3]).should_not have_json_fields([])
+    %(1).should_not have_json_fields([])
+    %("test").should_not have_json_fields([])
+  end
+
+  it "fails for non-complete set of fields" do
+    %({"a": "a", "b": "b"}).should_not have_json_fields(%w(a b c))
+  end
+
+  it "matches complete set of fields" do
+    %({"a": "a", "b": "b", "c": "c"}).should have_json_fields(%w(a b c))
+  end
+end
