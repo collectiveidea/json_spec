df4c09a9f362547e8d51a070d1564358c9c3f190
AlexanderPavlenko <a.pavlenko@roundlake.ru>
2012-11-14 21:16:11 +0400

    support cucumber have_json_keys matcher; improve tests; fix bug with table.rows

--- lib/json_spec/matchers/have_json_keys.rb
+++ lib/json_spec/matchers/have_json_keys.rb
@@ -1,17 +1,17 @@
 module JsonSpec
   module Matchers
-    class HaveJsonFields
+    class HaveJsonKeys
       include JsonSpec::Helpers
       include JsonSpec::Messages
 
-      def initialize(fields)
-        @fields = fields
+      def initialize(keys)
+        @keys = keys
       end
 
       def matches?(json)
         @data = parse_json(json, @path)
         return false unless @data.is_a?(Hash)
-        !@fields.map{|f| @data.has_key?(f.to_s)}.include?(false)
+        !@keys.map{|f| @data.has_key?(f.to_s)}.include?(false)
       end
 
       def at_path(path)
@@ -20,15 +20,15 @@ module JsonSpec
       end
 
       def failure_message_for_should
-        message_with_path("Expected JSON to contain all of the following fields #{@fields.join(", ")}")
+        message_with_path("Expected JSON to contain all of the following keys #{@keys.join(", ")}")
       end
 
       def failure_message_for_should_not
-        message_with_path("Expected JSON to not contain any of the following fields #{@fields.join(", ")}")
+        message_with_path("Expected JSON to not contain any of the following keys #{@keys.join(", ")}")
       end
 
       def description
-        message_with_path(%(have JSON fields "#{@fields.join(", ")}"))
+        message_with_path(%(have JSON keys "#{@keys.join(", ")}"))
       end
     end
   end
