1e36fc67d2df5f1dcb6aef57040bbfe793331c5b
nick.barone <nick.barone@daqri.com>
2013-06-28 16:52:39 -0700

    Include shows a diff

--- lib/json_spec/matchers/include_json.rb
+++ lib/json_spec/matchers/include_json.rb
@@ -5,6 +5,12 @@ module JsonSpec
       include JsonSpec::Exclusion
       include JsonSpec::Messages
 
+      attr_reader :expected, :actual
+
+      def diffable?
+        true
+      end
+
       def initialize(expected_json = nil)
         @expected_json = expected_json
       end
@@ -12,12 +18,12 @@ module JsonSpec
       def matches?(actual_json)
         raise "Expected included JSON not provided" if @expected_json.nil?
 
-        actual = parse_json(actual_json, @path)
-        expected = exclude_keys(parse_json(@expected_json))
-        case actual
-        when Hash then actual.values.map{|v| exclude_keys(v) }.include?(expected)
-        when Array then actual.map{|e| exclude_keys(e) }.include?(expected)
-        when String then actual.include?(expected)
+        @actual = scrub(actual_json, @path)
+        @expected = scrub(@expected_json)
+        case @actual
+        when Hash then @actual.values.map{|v| exclude_keys(v) }.include?(@expected)
+        when Array then @actual.map{|e| exclude_keys(e) }.include?(@expected)
+        when String then @actual.include?(@expected)
         else false
         end
       end
@@ -53,6 +59,11 @@ module JsonSpec
       def description
         message_with_path("include JSON")
       end
+
+      private
+        def scrub(json, path = nil)
+          generate_normalized_json(exclude_keys(parse_json(json, path))).chomp + "\n"
+        end
     end
   end
 end
